1a65ee59cac35bb6d9ef9d7e818267c90e5162cd
ahmadhuss <ahmadnet600@gmail.com>
2021-04-12 23:03:24 +0500

    refactor: app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Laravel</title>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
     <nav class="app-navbar">
@@ -14,16 +14,16 @@
             <ul class="app-navbar__list">
                 @guest
                 <li>
-                    <a href="#">Signin</a>
+                    <a href="/signin">Signin</a>
                 </li>
                 <li>
-                    <a href="{{ route('signup') }}">Signup</a>
+                    <a href="/signup">Signup</a>
                 </li>
                 @endguest
 
                 @auth
                   <li>
-                   <a href="{{ route('home') }}">Home</a>
+                   <a href="/">Home</a>
                   </li>
 
                   <li>
