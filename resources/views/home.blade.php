c252ef62887164aebaa1f16eaee15174d1ae13b1
ahmadhuss <ahmadnet600@gmail.com>
2021-04-14 23:14:24 +0500

    feat: Added the protected route link button i.e. creation of invoices

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,10 @@
     <div class="text-center">
         <h1>Hello{{ auth()->user() ? ', '. auth()->user()->name : '' }}</h1>
         <p>Welcome to the Home</p>
+        @auth
+        <div>
+            <a href="{{ route('invoices.create') }}" class="btn btn-primary">Create Invoice</a>
+        </div>
+        @endauth
     </div>
 @endsection
