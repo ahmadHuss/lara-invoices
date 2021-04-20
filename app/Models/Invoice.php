e237a95aa70c082451201edf467adbb34a96c0c4
ahmadhuss <ahmadnet600@gmail.com>
2021-04-20 12:12:43 +0500

    feat: Invoice Model belongsTo 1:M relation has been created

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -10,4 +10,10 @@ class Invoice extends Model
     use HasFactory;
 
     protected $fillable = ['invoice_number', 'invoice_date', 'customer_id', 'tax_percent'];
+
+    // belongsTo represents 1 to M => 1Customer can create many invoices
+    public function customer() {
+        // Customer::class must return a relationship instance
+        return $this->belongsTo(Customer::class);
+    }
 }
