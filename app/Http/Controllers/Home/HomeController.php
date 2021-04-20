54d66006b2b5248cf61f7a5d128c0625a3078006
ahmadhuss <ahmadnet600@gmail.com>
2021-04-20 12:13:53 +0500

    refactor: HomeController.php collection query has been passed into view

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function __construct() {
+        $this->middleware(['auth']);
+    }
 
+    public function index() {
+        $invoices = Invoice::with('customer')->get(); // returns the collection object
+        return view('home', compact('invoices'));
+    }
 }
