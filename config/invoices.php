aa0e4554f870c30426eedab3ad8bff97ab04c6f2
ahmadhuss <ahmadnet600@gmail.com>
2021-04-22 19:27:57 +0500

    feat: config seller details are added

---
+++ config/invoices.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'footer_text' => 'Invoice must be paid within 30 days.',
+    'logo_file' => '/images/logo.png',
+    'seller' => [
+        'name' => 'Your company name',
+        'address' => '1 street name, London, United Kingdom',
+        'email' => '',
+        'additional_info' => [
+            'VAT Number' => 'XX XXX XXX'
+        ]
+    ]
+];
