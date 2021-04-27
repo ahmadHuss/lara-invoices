9d36b0f2fcd18a52b1178873e96c981d9f7b4bb7
ahmadhuss <ahmadnet600@gmail.com>
2021-04-27 15:34:34 +0500

    refactor: home.blade.php not show unnecessary table headings

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,29 +8,36 @@
             <a href="{{ route('invoices.create') }}" class="btn btn-primary">Create Invoice</a>
         </div>
 
-        {{-- Responsive Tables --}}
-        <div class="table-responsive mt-5">
-                <table class="table table-bordered">
-                    <tr>
-                        <th>Invoice Date</th>
-                        <th>Invoice Number</th>
-                        <th>Customer</th>
-                        <th>Total Amount</th>
-                        <th></th>
-                    </tr>
-                    @foreach($invoices as $invoice)
+        @if (count($invoices) > 0)
+            {{-- Responsive Tables --}}
+            <div class="table-responsive mt-5">
+                    <table class="table table-bordered">
                         <tr>
-                            <td>{{ $invoice->invoice_date }}</td>
-                            <td>{{ $invoice->invoice_number }}</td>
-                            <td>{{ $invoice->customer->name }}</td>
-                            <td>{{ number_format($invoice->total_amount, 2) }}</td>
-                            <td>
-                                <a href="{{ route('invoices.show', $invoice->id) }}" class="btn btn-primary">View Invoice</a>
-                            </td>
+                            <th>Invoice Date</th>
+                            <th>Invoice Number</th>
+                            <th>Customer</th>
+                            <th>Total Amount</th>
+                            <th></th>
+                            <th></th>
                         </tr>
-                    @endforeach
-                </table>
-        </div>
+                        @foreach($invoices as $invoice)
+                            <tr>
+                                <td>{{ $invoice->invoice_date }}</td>
+                                <td>{{ $invoice->invoice_number }}</td>
+                                <td>{{ $invoice->customer->name }}</td>
+                                <td>{{ number_format($invoice->total_amount, 2) }}</td>
+                                <td>
+                                    <a href="{{ route('invoices.show', $invoice->id) }}" class="btn btn-primary">View Invoice</a>
+                                </td>
+
+                                <td>
+                                    <a href="{{ route('invoices.download', $invoice->id) }}" class="btn btn-secondary">Download PDF</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+            </div>
+         @endif
         @endauth
     </div>
 @endsection
