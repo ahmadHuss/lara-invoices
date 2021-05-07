1cb17c41d2ced9bd2eb95495a76815ae2eaebfb1
ahmadhuss <ahmadnet600@gmail.com>
2021-05-08 00:26:08 +0500

    refactor: Added functionality to get the customer_id
    * We have added a functionality to get the `customers_id` so we can take an customer

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 @section('section')
-    <div class="app-box" style="max-width: 1300px;">
+    <div class="app-box" style="max-width: 1365px;">
         <div class="text-center">
             <h1>Hello{{ auth()->user() ? ', '. auth()->user()->name : '' }}</h1>
             <p>Do you want to add new customer?</p>
@@ -22,6 +22,7 @@
                                 <th>Country</th>
                                 <th>Phone</th>
                                 <th>Email</th>
+                                <th></th>
                             </tr>
                             @foreach($customers as $customer)
                                 <tr>
@@ -35,6 +36,12 @@
                                     <td>{{ $customer->email }}</td>
                                     {{-- We don't want to delete the customer whole database and
                                      invoice will be affected!  --}}
+
+                                    {{--------------------------- }}
+
+                                    {{-- We want to get the `country` of a customer in create invoices link. --}}
+                                    <td><a href="{{ route('invoices.create') }}?customer_id={{$customer->id}}" class="btn btn-primary">Create Invoice</a></td>
+
                                 </tr>
                             @endforeach
                         </table>
